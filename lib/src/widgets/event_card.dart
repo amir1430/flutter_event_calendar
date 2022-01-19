39139632fa0407549c855b0a07789197e8b4b255
novadaycompany <hosein1398@gmail.com>
2022-01-19 14:27:32 +0330

    feat: remove restrict child set & just get child directly for event

--- lib/src/widgets/event_card.dart
+++ lib/src/widgets/event_card.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_event_calendar/src/handlers/event_calendar.dart';
-import 'package:flutter_event_calendar/src/models/calendar_options.dart';
 import 'package:flutter_event_calendar/src/models/event.dart';
 import 'package:flutter_event_calendar/src/models/style/event_options.dart';
 
@@ -20,56 +18,7 @@ class EventCard extends StatelessWidget {
         onLongPress: (() {
           fullCalendarEvent.onLongPress?.call(fullCalendarEvent.listIndex);
         }),
-        child: Card(
-          color: EventOptions.of(context).backgroundColor,
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: EventCalendar.calendarProvider.isRTL()
-                  ? CrossAxisAlignment.end
-                  : CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '${fullCalendarEvent.title}',
-                  textDirection: EventCalendar.calendarProvider.isRTL()
-                      ? TextDirection.rtl
-                      : TextDirection.ltr,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                    color: EventOptions.of(context).titleColor,
-                    fontFamily: CalendarOptions.of(context).font,
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  '${fullCalendarEvent.description}',
-                  textDirection: EventCalendar.calendarProvider.isRTL()
-                      ? TextDirection.rtl
-                      : TextDirection.ltr,
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: EventOptions.of(context).descriptionColor,
-                    fontFamily: CalendarOptions.of(context).font,
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  '${fullCalendarEvent.dateTime.toString()}',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: EventOptions.of(context).dateTimeColor,
-                    fontFamily: CalendarOptions.of(context).font,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        child: fullCalendarEvent.child,
       ),
     );
   }
