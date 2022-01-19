3315e36ddcd0ea98d292251c836e27cb344783ab
novadaycompany <hosein1398@gmail.com>
2022-01-19 14:27:52 +0330

    chore: remove redundant styles

--- lib/src/models/style/event_options.dart
+++ lib/src/models/style/event_options.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class EventOptions extends Model {
@@ -7,20 +6,13 @@ class EventOptions extends Model {
   Color emptyTextColor;
   IconData emptyIcon;
   Color emptyIconColor;
-  Color backgroundColor;
-  Color titleColor;
-  Color descriptionColor;
-  Color dateTimeColor;
 
-  EventOptions(
-      {this.emptyText,
-      this.emptyTextColor = const Color(0xffe5e5e5),
-      this.emptyIcon = Icons.reorder,
-      this.emptyIconColor = const Color(0xffebebeb),
-      this.backgroundColor = Colors.white,
-      this.titleColor = Colors.black,
-      this.descriptionColor = Colors.grey,
-      this.dateTimeColor = Colors.grey});
+  EventOptions({
+    this.emptyText,
+    this.emptyTextColor = const Color(0xffe5e5e5),
+    this.emptyIcon = Icons.reorder,
+    this.emptyIconColor = const Color(0xffebebeb),
+  });
 
   static EventOptions of(BuildContext context) =>
       ScopedModel.of<EventOptions>(context);
