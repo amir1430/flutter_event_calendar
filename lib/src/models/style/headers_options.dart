9313ca8c23e72b93fab8446bcc4f5554f200a4db
novadaycompany <hosein1398@gmail.com>
2022-01-19 14:28:34 +0330

    chore: add header styles

--- lib/src/models/style/headers_options.dart
+++ lib/src/models/style/headers_options.dart
@@ -5,11 +5,16 @@ import 'package:scoped_model/scoped_model.dart';
 class HeaderOptions extends Model {
   WeekDayStringTypes weekDayStringType;
   MonthStringTypes monthStringType;
+  Color headerTextColor;
+  Color navigationColor;
+  Color resetDateColor;
 
-  HeaderOptions({
-    this.weekDayStringType = WeekDayStringTypes.FULL,
-    this.monthStringType = MonthStringTypes.SHORT,
-  });
+  HeaderOptions(
+      {this.weekDayStringType = WeekDayStringTypes.FULL,
+      this.monthStringType = MonthStringTypes.SHORT,
+      this.headerTextColor = Colors.black,
+      this.resetDateColor = Colors.black,
+      this.navigationColor = Colors.black});
 
   static HeaderOptions of(BuildContext context) =>
       ScopedModel.of<HeaderOptions>(context);
