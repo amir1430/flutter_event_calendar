965017f73735c9b8c1f3cf7005b3719d4af25e86
novadaycompany <hosein1398@gmail.com>
2022-01-19 14:26:06 +0330

    chore: add showWeekDay & mini mode option

--- lib/src/models/style/day_options.dart
+++ lib/src/models/style/day_options.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class DayOptions extends Model{
+class DayOptions extends Model {
   Color weekDaySelectedColor;
   Color weekDayUnselectedColor;
+  bool showWeekDay;
+  bool mini;
   Color selectedBackgroundColor;
   Color unselectedBackgroundColor;
   Color selectedTextColor;
@@ -19,12 +21,13 @@ class DayOptions extends Model{
       this.selectedBackgroundColor = const Color(0xff3AC3E2),
       this.unselectedBackgroundColor = Colors.transparent,
       this.selectedTextColor = Colors.white,
+      this.showWeekDay = true,
+      this.mini = false,
       this.unselectedTextColor = Colors.black,
-      this.eventCounterColor =  Colors.red,
+      this.eventCounterColor = Colors.red,
       this.eventCounterViewType = DayEventCounterViewType.LABEL,
       this.eventCounterTextColor = Colors.white});
 
   static DayOptions of(BuildContext context) =>
       ScopedModel.of<DayOptions>(context);
-
 }
